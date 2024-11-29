63684c46908af219a5145eb0a95d0dad833707d4
Patrick Bélanger <pbelanger@hotmail.com>
2024-11-29 08:35:55 -0500

    1st commit of the Windows app

---
+++ PostToPoint.Windows/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PostToPoint.Windows
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
