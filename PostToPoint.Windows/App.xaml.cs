f9b48be5de27edd1ea1c1d87f82054d85156bda0
Patrick Bélanger <pbelanger@hotmail.com>
2024-11-29 09:14:17 -0500

    Added args support

--- PostToPoint.Windows/App.xaml.cs
+++ PostToPoint.Windows/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Configuration;
+﻿using CommandLine;
+using CommandLine.Text;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +11,57 @@ namespace PostToPoint.Windows
     /// </summary>
     public partial class App : Application
     {
+        public static CommandLineOptions Options = new CommandLineOptions();
+
+        [STAThread]
+        public static void Main2(string[] args)
+        {
+            var application = new App();
+            application.InitializeComponent();
+            application.Run();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Explicitly declare the result variable
+            ParserResult<CommandLineOptions> result = Parser.Default.ParseArguments<CommandLineOptions>(e.Args);
+
+            // Parse the command line arguments
+            result
+                .WithParsed(opts =>
+                {
+                    // This code runs if all required parameters are provided
+                    // and parsing succeeds
+
+                    //if (opts.Verbose)
+                    //{
+                    //    Console.WriteLine($"Processing file: {opts.InputFile}");
+                    //    Console.WriteLine($"Port: {opts.Port}");
+
+                    //    if (opts.OutputFile != null)
+                    //        Console.WriteLine($"Output file: {opts.OutputFile}");
+
+                    //    if (opts.Tags != null)
+                    //        Console.WriteLine($"Tags: {string.Join(", ", opts.Tags)}");
+                    //}
+
+                    Options = opts;
+                })
+                .WithNotParsed(errors =>
+                {
+                    // This code runs if there are any parsing errors
+                    // (like missing required parameters)
+
+                    var helpText = HelpText.AutoBuild(result);
+                    Console.WriteLine(helpText);
+
+                    // You might want to exit the application or show an error dialog
+                    MessageBox.Show("Invalid command line arguments. Check --help for usage.");
+                    Environment.Exit(1);
+                });
+        }
     }
 
 }
---
+++ PostToPoint.Windows/CommandLineOptions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace PostToPoint.Windows
+{
+    public class CommandLineOptions
+    {
+        // Response file support
+        [Value(0)]
+        public IEnumerable<string> ResponseFiles { get; set; }
+
+        [Option("reddit-username", Required = false, HelpText = "Your Reddit username")]
+        public string RedditUsername { get; set; }
+
+        [Option("reddit-password", Required = false, HelpText = "Your Reddit password")]
+        public string RedditPassword { get; set; }
+
+        [Option("reddit-client-id", Required = false, HelpText = "Your Reddit client ID")]
+        public string RedditAppId { get; set; }
+
+        [Option("reddit-client-secret", Required = false, HelpText = "Your Reddit client secret")]
+        public string RedditAppSecret { get; set; }
+
+
+    }
+}
