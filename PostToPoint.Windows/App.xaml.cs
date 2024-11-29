67e2e4d35a4652d2a1b47c3809e16b2293d3a7b2
Patrick Bélanger <pbelanger@hotmail.com>
2024-11-29 09:45:13 -0500

    Better command line args support for files

--- PostToPoint.Windows/App.xaml.cs
+++ PostToPoint.Windows/App.xaml.cs
@@ -2,6 +2,9 @@
 using CommandLine.Text;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 using System.Windows;
 
 namespace PostToPoint.Windows
@@ -25,9 +28,24 @@ namespace PostToPoint.Windows
         {
             base.OnStartup(e);
 
-            // Explicitly declare the result variable
-            ParserResult<CommandLineOptions> result = Parser.Default.ParseArguments<CommandLineOptions>(e.Args);
+            // Parse the command line arguments to replace args with the contents of config files
+            // if an argument starts with @, read the file and replace the argument with the contents of the file
+            List<string> args = new List<string>();
+            foreach(var arg in e.Args)
+            {
+                if (arg.StartsWith("@"))
+                {
+                    args.AddRange(File.ReadAllLines(arg.Substring(1)));
+                }
+                else
+                {
+                    args.Add(arg);
+                }
+            }
 
+            // Explicitly declare the result variable
+            ParserResult<CommandLineOptions> result = Parser.Default.ParseArguments<CommandLineOptions>(args);
+            
             // Parse the command line arguments
             result
                 .WithParsed(opts =>
@@ -51,11 +69,16 @@ namespace PostToPoint.Windows
                 })
                 .WithNotParsed(errors =>
                 {
-                    // This code runs if there are any parsing errors
-                    // (like missing required parameters)
-
-                    var helpText = HelpText.AutoBuild(result);
+                    // This code runs if there are any parsing errors (like missing required parameters)
+                    var helpText = HelpText.AutoBuild(result, h => 
+                    {
+                        h.AdditionalNewLineAfterOption = false;
+                        h.Heading = "PostToPoint v" + Assembly.GetExecutingAssembly().GetName().Version;
+                        h.AddPostOptionsLine("You can place all your arguments in a file, simply passing @file.ext in place of the arguments.  Can also be combined with real CLI arguments, the file will be expanded 'in place' where you put it.");
+                        return HelpText.DefaultParsingErrorsHandler(result, h);
+                    });
                     Console.WriteLine(helpText);
+                    Debug.WriteLine(helpText);
 
                     // You might want to exit the application or show an error dialog
                     MessageBox.Show("Invalid command line arguments. Check --help for usage.");
--- PostToPoint.Windows/CommandLineOptions.cs
+++ PostToPoint.Windows/CommandLineOptions.cs
@@ -9,22 +9,16 @@ namespace PostToPoint.Windows
 {
     public class CommandLineOptions
     {
-        // Response file support
-        [Value(0)]
-        public IEnumerable<string> ResponseFiles { get; set; }
-
         [Option("reddit-username", Required = false, HelpText = "Your Reddit username")]
         public string RedditUsername { get; set; }
 
         [Option("reddit-password", Required = false, HelpText = "Your Reddit password")]
         public string RedditPassword { get; set; }
 
-        [Option("reddit-client-id", Required = false, HelpText = "Your Reddit client ID")]
+        [Option("reddit-app-id", Required = false, HelpText = "Your Reddit client ID")]
         public string RedditAppId { get; set; }
 
-        [Option("reddit-client-secret", Required = false, HelpText = "Your Reddit client secret")]
+        [Option("reddit-app-secret", Required = false, HelpText = "Your Reddit client secret")]
         public string RedditAppSecret { get; set; }
-
-
     }
 }
